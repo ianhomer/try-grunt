28c77b4d8344f20bd1524d2aa1182e68e0c7eb7f
Ian Homer <ian.homer@gmail.com>
2018-04-15 22:18:03 +0100

    Extend uglify task to include git hash

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,34 @@
 module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    githash: {
+      main: {
+        options: {},
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> @ <%= githash.main.short %> ' +
+          '(<%=githash.main.branch %>) */\n'
+      },
+      build: {
+        src: 'src/my.js',
+        dest: 'build/my.min.js'
+      }
+    }
+  });
 
-  // Basic default task.
-  grunt.registerTask('default', 'Logging', function() {
+  // Load NPM plugins
+  grunt.loadNpmTasks('grunt-githash');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Inline task
+  grunt.registerTask('log', 'Logging task', function() {
     grunt.log.write('Trying grunt\n').ok();
   });
+
+  // Basic default task.
+  grunt.registerTask('default', ['log', 'githash', 'uglify']);
 
 };
